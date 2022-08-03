538c84b133dc2ebc11dd61fa127787a4c0a86cff
Shamso Osman <94788755+S-Osman4@users.noreply.github.com>
2022-08-03 18:33:33 +0300

    Public class attribute: State

---
+++ models/state.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+State class module
+"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """class State which inherits from class BaseModel"""
+    name = ""
