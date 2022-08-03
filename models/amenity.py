e7698495f93c6abd93d1e4dfcfed715a519762ba
Shamso Osman <94788755+S-Osman4@users.noreply.github.com>
2022-08-03 18:26:09 +0300

    Public class attribute: Amenity

---
+++ models/amenity.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+"""
+Amenity class module
+"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """class Amenity which inherits from BaseModel"""
+    name = ""
