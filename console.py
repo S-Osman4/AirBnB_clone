a6045902096c6a98e2f99d11162b1c1aca4316cf
Shamso Osman <94788755+S-Osman4@users.noreply.github.com>
2022-08-05 18:38:09 +0300

    entry point of the command interpreter

---
+++ console.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""This is the console for AirBnB"""
+import cmd
+from models import storage
+from datetime import datetime
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+import shlex
+
+
+class HBNBCommand(cmd.Cmd):
+    """this class is entry point of the command interpreter
+    """
+    prompt = "(hbnb) "
+    all_classes = {"BaseModel", "User", "State", "City",
+                   "Amenity", "Place", "Review"}
+
+    def emptyline(self):
+        """Ignores empty spaces"""
+        pass
+
+    def do_quit(self, line):
+        """Quit command to exit the program"""
+        return True
+
+    def do_EOF(self, line):
+        """Quit command to exit the program at end of file"""
+        return True
+        
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
